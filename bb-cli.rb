4bd56b85ebfe63faa824c43450eaf0b99c6c0438
Julian Castano <julian@castano.app>
2024-12-24 13:59:14 -0500

    Add bb-cli v0.1.0 formula

---
+++ bb-cli.rb
@@ -0,0 +1,17 @@
+class BbCli < Formula
+  desc "Command-line tool for interacting with Bitbucket repositories"
+  homepage "https://github.com/TheChessDev/bb-cli"
+  url "https://github.com/TheChessDev/bb-cli/archive/v0.1.0.tar.gz"
+  sha256 "4b6cb97460f32a92d1aa96f2d909836a34ff1a23e545f9f83b6bc5d4770c06e8"
+  license "MIT"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args, "./cmd/bb"
+  end
+
+  test do
+    assert_match "bb version", shell_output("#{bin}/bb version")
+  end
+end
