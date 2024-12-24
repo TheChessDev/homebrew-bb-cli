58a7462758f2d61e35268505731222452b054c97
Julian Castano <julian@castano.app>
2024-12-24 14:02:16 -0500

    Update build

--- bb-cli.rb
+++ bb-cli.rb
@@ -8,7 +8,7 @@ class BbCli < Formula
   depends_on "go" => :build
 
   def install
-    system "go", "build", *std_go_args, "./cmd/bb"
+    system "go", "build", *std_go_args, "./"
   end
 
   test do
