0b1a40b06c2c247bad7a68e195846853017a4a95
Julian Castano <julian@castano.app>
2024-12-24 14:44:38 -0500

    chore: update bb-cli formula to v0.3.1

--- bb-cli.rb
+++ bb-cli.rb
@@ -1,8 +1,8 @@
 class BbCli < Formula
   desc "Command-line tool for interacting with Bitbucket repositories"
   homepage "https://github.com/TheChessDev/bb-cli"
-  url "https://github.com/TheChessDev/bb-cli/archive/v0.2.0.tar.gz"
-  sha256 "20a4f06e5caf9469be66a1bcef03d0e82455542eff137f241a7378f5c1ab68d4"
+  url "https://github.com/TheChessDev/bb-cli/archive/v0.3.1.tar.gz"
+  sha256 "4844819b335cb4110db67ac9fb8c4b6809dfd293875149ab1c5da8a4f633de77"
   license "MIT"
 
   depends_on "go" => :build
